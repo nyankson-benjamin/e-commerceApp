cec2bca470551d53f8274764c87c59d4443bfb63
nyankson-benjamin <benjamin.nyankson@amalitech.com>
2023-03-15 17:15:42 -0700

    Update Forgot.js

--- src/Pages/Forgot.js
+++ src/Pages/Forgot.js
@@ -1,12 +1,50 @@
 import React from "react";
-import ResetPassword from "../components/User/ForgetPassword";
-import { AppBar } from "@mui/material";
+import ForgetPassword from "../components/User/ForgetPassword";
+import { AppBar, Box } from "@mui/material";
+import ForgotImg from "../Assets/user/ForgotImg.jpg";
 import Footer from "../components/Footer";
-export default function Reset() {
+import useScreenWidth from "../Hooks/useScreenWidth";
+import AppsBar from "../TopBar/AppBar";
+export default function Forgot() {
+  const [screenWidth] = useScreenWidth();
   return (
     <div>
-      <AppBar />
-      <ResetPassword />
+      <AppsBar />
+      {screenWidth > 600 ? (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            m: "auto",
+            width: "90%",
+          }}
+        >
+          <Box sx={{ width: "40%" }}>
+            <img src={ForgotImg} alt="" style={{ width: "100%" }} />
+          </Box>
+          <Box sx={{ width: "50%",  }}>
+            <ForgetPassword />
+          </Box>
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            m: "auto",
+            width: "90%",
+          }}
+        >
+          {/* <Box sx={{ width: "100%" }}>
+            <img src={ForgotImg} alt="" style={{ width: "100%" }} />
+          </Box> */}
+          <Box sx={{ width: "100%" }}>
+            <ForgetPassword />
+          </Box>
+        </Box>
+      )}
     </div>
   );
 }
