4bbb60abe4a17f7d44bb6b7092f1a8b0ab0fa6c1
nyankson-benjamin <benjamin.nyankson@amalitech.com>
2023-03-20 17:40:55 -0700

    Create UserTopBar.jsx

---
+++ src/TopBar/UserTopBar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Box } from "@mui/system";
+export default function UserTopBar() {
+  return (
+    <Box sx={{ width: "100%", height: "100px", bgcolor: "#ffdf28" }}>
+      userTopBar
+    </Box>
+  );
+}
