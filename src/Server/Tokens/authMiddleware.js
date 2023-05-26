433fc074a824efab31acc75e2e24c87916c8775b
nyankson-benjamin <117347759+nyankson-benjamin@users.noreply.github.com>
2023-05-26 09:09:57 +0000

    feat: verify users token

---
+++ src/Server/Tokens/authMiddleware.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+const requireAuth = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  //check if there is a token and is verified
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        res.redirect("/login");
+      } else {
+        console.log("decodedToken", decodedToken);
+        next();
+      }
+    });
+  } else {
+    res.redirect("/login");
+  }
+};
+
+module.exports = requireAuth;
